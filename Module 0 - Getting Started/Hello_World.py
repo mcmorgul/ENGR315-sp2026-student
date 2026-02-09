a1dc8b230e02ca1acd87b39a22bbda9b5145482a
M Ceylan Morgul <mm4uz@virginia.edu>
2026-02-09 15:10:46 -0500

    Update Hello_World.py

--- Module 0 - Getting Started/Hello_World.py
+++ Module 0 - Getting Started/Hello_World.py
@@ -4,6 +4,7 @@
 
 # Below this line is going to be a "print" function
 print("Hello, World!")
+print()
 
 # Right Click in this Window, and click "Run Hello_World.py"
 # Then Look down to the bottom to see what happens!
